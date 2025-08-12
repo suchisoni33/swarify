cbe4f19b03dbc70790da642bafd5dd1b84fa0b97
suchi soni <suchisoni33@gmail.com>
2025-08-12 12:43:58 +0530

    Fix case sensitivity for Sidebar import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Sidebar from './components/sidebar';
+
+import Sidebar from './components/Sidebar';
 import Player from './components/Player';
 import Display from './components/Display';
 import {PlayerContext} from './context/PlayerContext';
